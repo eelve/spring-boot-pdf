747d978745c8213064bffde5c06bfd3b14a645d7
eelve <zzlve@foxmail.com>
2020-04-24 20:50:15 +0800

    添加项目架构

---
+++ src/test/java/com/eelve/springbootpdf/SpringBootPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eelve.springbootpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
