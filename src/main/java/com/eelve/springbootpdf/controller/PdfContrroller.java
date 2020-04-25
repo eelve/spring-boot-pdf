4a82f2e796fbe3ac5b04e06237679aca82bb7e76
eelve <zzlve@foxmail.com>
2020-04-25 11:10:05 +0800

    添加PathVariable传参方法

--- src/main/java/com/eelve/springbootpdf/controller/PdfContrroller.java
+++ src/main/java/com/eelve/springbootpdf/controller/PdfContrroller.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,6 +29,44 @@ public class PdfContrroller {
 
     @RequestMapping(value = "/preview")
     public void dashboardView(HttpServletRequest request, HttpServletResponse response, String id){
+        log.info("文件id为：" + id);
+        File file = null;
+        try{
+            switch (id){
+                case "1" :
+                    file = ResourceUtils.getFile("classpath:泰山版Java开发手册.pdf");
+                    break;
+                case "2" :
+                    String filePath = getClass().getResource("/").getPath()+"eelve.pdf";
+                    file = ResourceUtils.getFile(filePath);
+                    break;
+                default:
+                    file = ResourceUtils.getFile("classpath:eelve.pdf");
+                    break;
+            }
+
+            if (file.exists()) {
+                byte[] data = null;
+                FileInputStream input=null;
+
+                input= new FileInputStream(file);
+                data = new byte[input.available()];
+                input.read(data);
+                response.getOutputStream().write(data);
+            }
+        }catch (Exception e){
+            log.info( "Exception"+e);
+        }
+    }
+
+    /**
+     * 使用PathVariable取值
+     * @param request
+     * @param response
+     * @param id
+     */
+    @RequestMapping(value = "/view/{id}")
+    public void view(HttpServletRequest request, HttpServletResponse response, @PathVariable(value="id") String id){
         log.info("文件id为：" + id);
         File file = null;
         try{
